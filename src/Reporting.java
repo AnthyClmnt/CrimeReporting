d445133bf7b027220509ab93b3ba9e2d3167971e
Anthony <Anthony.clermont@outlook.com>
2021-02-16 18:21:35 +0000

    Creation of the Reporting class, added functionality to find the crime with the highest value theft.

--- src/Reporting.java
+++ src/Reporting.java
@@ -1,2 +1,22 @@
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Reporting {
+    public Reporting(String opt, List<Incident> incidentList) {
+        AtomicInteger max_value = new AtomicInteger();
+        if (opt.equals("highestValue")) {
+            incidentList.forEach(distName ->{
+                String value = distName.getValue();
+                int intValue = Integer.parseInt(value);
+                if (max_value.get() < intValue) {
+                    max_value.set(intValue);
+                }
+            });
+            System.out.println("Max Value: £" + max_value);
+        }
+
+        if (opt.equals("averageValue")) {
+            System.out.println(opt);
+        }
+    }
 }
