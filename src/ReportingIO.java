0418f2aa6634a3ce6286cb78b1a5c1beb8acc36a
Anthony <Anthony.clermont@outlook.com>
2021-02-16 18:20:46 +0000

    Changes made to the menu of the system, to allow the Stats option to have functionality. Added search function which allows user to select a desired statistic, and calls the Reporting class. Along with minor erroneous lines of code and bug fixes.

--- src/ReportingIO.java
+++ src/ReportingIO.java
@@ -1,10 +1,28 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class ReportingIO {
     public static void main(String[] args) throws FileNotFoundException {
-        System.out.println("hello");
+        District newDist1 = new District("Newcastle", "Murder");
+        District newDist2 = new District("Durham", "Robbery");
+        District newDist3 = new District("Durham", "Robbery");
+
+        Incident newInc1 = new Incident("NE4 5SN", "100", "10/02/21");
+        Incident newInc2 = new Incident("NE4 9SN", "6700", "03/01/21");
+        Incident newInc3 = new Incident("TS18 2SY", "12500", "27/11/20");
+
+        List<District> districtList = new ArrayList<>();
+        districtList.add(newDist1);
+        districtList.add(newDist2);
+        districtList.add(newDist3);
+
+        List<Incident> incidentList = new ArrayList<>();
+        incidentList.add(newInc1);
+        incidentList.add(newInc2);
+        incidentList.add(newInc3);
 
         Scanner s = new Scanner(System.in);
         boolean quit = false;
@@ -15,9 +33,9 @@ public class ReportingIO {
             }
             String option = s.nextLine();
             switch (option) {
-                case "1" -> enterDistrictInfo();
-                case "2" -> enterIncidentInfo();
-                case "3" -> System.out.println("You chose option 3");
+                case "1" -> enterDistrictInfo(districtList);
+                case "2" -> enterIncidentInfo(incidentList);
+                case "3" -> search(districtList, incidentList);
                 case "4" -> {
                     System.out.println("You chose to quit");
                     quit = true;
@@ -27,7 +45,7 @@ public class ReportingIO {
         }
     }
 
-    private static void enterDistrictInfo() {
+    private static void enterDistrictInfo(List<District> districtList) {
         Scanner s = new Scanner(System.in);
         System.out.println("--- Enter District Info ---");
         System.out.print("District Name: ");
@@ -36,11 +54,10 @@ public class ReportingIO {
         String crimeType = s.nextLine();
 
         District newDistrict = new District(distName, crimeType);
-        //test
-        System.out.println(newDistrict.getDistrictName());
+        districtList.add(newDistrict);
     }
 
-    private static void enterIncidentInfo() {
+    private static void enterIncidentInfo(List<Incident> incidentList) {
         Scanner s = new Scanner(System.in);
         System.out.println("--- Enter Incident Info ---");
         System.out.print("Postcode: ");
@@ -51,9 +68,39 @@ public class ReportingIO {
         String date = s.nextLine();
 
         Incident newIncident = new Incident(postcode, value, date);
-        //test
-        System.out.println(newIncident.getValue());
-        System.out.println(newIncident.getPostcode());
-        System.out.println(newIncident.getDate());
+        incidentList.add(newIncident);
+    }
+
+    private static void search(List<District> districtList, List<Incident> incidentList) throws FileNotFoundException {
+        Scanner s = new Scanner(System.in);
+        boolean returnToMenu = false;
+        while (!returnToMenu) {
+            System.out.println("------Menu------\n" +
+                    "1: Highest Value Crime\n" +
+                    "2: Average Value Crime\n" +
+                    "3: Return to main menu\n" +
+                    "----------------");
+
+            String option = s.nextLine();
+            switch (option) {
+                case "1" -> new Reporting("highestValue", incidentList);
+                case "2" -> new Reporting("averageValue", incidentList);
+                case "3" -> { returnToMenu = true;
+                }
+                default -> System.out.println("Invalid Option!!");
+            }
+        }
+
+        /*System.out.println("--- Enter District Name ---");
+        System.out.print("District Name: ");
+        String disAsked = s.nextLine();
+
+        districtList.forEach(distName ->{
+            if (disAsked.equals(distName.getDistrictName())){
+                System.out.println(distName.getIncident());
+            }
+        });
+
+         */
     }
 }
