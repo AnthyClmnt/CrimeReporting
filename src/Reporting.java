0e310b135bd7bc003c2ce7eebf3e131f4d99d309
Anthony <Anthony.clermont@outlook.com>
2021-02-17 10:27:51 +0000

    Added two functions of the reporting class. Firstly, one which displays all incidents with a value greater than a given amount. Secondly, a function which displays the average incident value in a given year. Along with two more imports, that were needed.

--- src/Reporting.java
+++ src/Reporting.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Reporting {
@@ -15,8 +17,44 @@ public class Reporting {
             System.out.println("Max Value: £" + max_value);
         }
 
-        if (opt.equals("averageValue")) {
-            System.out.println(opt);
+        if (opt.equals("incident>£x")) {
+            Scanner s = new Scanner(System.in);
+            System.out.println("--- Enter Value ---");
+            System.out.print("Minimum Value: £");
+            String inpValue = s.nextLine();
+
+            System.out.println("List of Incidents: ");
+            incidentList.forEach(distName ->{
+                if (Integer.parseInt(inpValue) < Integer.parseInt(distName.getValue())) {
+                    System.out.println("\n Postcode:" + distName.getPostcode() + "\n Value: " + distName.getValue() + "\n Date: " + distName.getDate());
+                    System.out.println(" ");
+                }
+            });
+        }
+
+        if (opt.equals("averageIncidentInYear")) {
+            ArrayList<Integer> totalValues = new ArrayList<Integer>();
+
+            Scanner s = new Scanner(System.in);
+            System.out.println("--- Enter Year ---");
+            System.out.print("Crime Year: ");
+            String inpYear = s.nextLine();
+
+            System.out.println("List of Incidents: ");
+            incidentList.forEach(distName ->{
+                String dateInfo = distName.getDate();
+                String yearInfo = dateInfo.substring(6, 10);
+                if (inpYear.equals(yearInfo)){
+                    //System.out.println("\n Postcode:" + distName.getPostcode() + "\n Value: " + distName.getValue() + "\n Date: " + distName.getDate());
+                    //System.out.println(" ");
+                    totalValues.add(Integer.parseInt(distName.getValue()));
+                }
+            });
+            int totalSum = 0;
+            for (int value : totalValues) {
+                totalSum += value;
+            }
+            System.out.println("Average value for year " + inpYear + " is: £" + totalSum);
         }
     }
 }
