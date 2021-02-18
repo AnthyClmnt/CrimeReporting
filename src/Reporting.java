652e8ad18a62e17e28b60096bcd37d18ec45c328
Anthony <Anthony.clermont@outlook.com>
2021-02-18 11:38:34 +0000

    Alteration to methods, as the incident class was changed, simple fixed to produce same outcome.

--- src/Reporting.java
+++ src/Reporting.java
@@ -5,8 +5,8 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class Reporting {
     public Reporting(String opt, List<Incident> incidentList) {
-        AtomicInteger max_value = new AtomicInteger();
         if (opt.equals("highestValue")) {
+            AtomicInteger max_value = new AtomicInteger();
             incidentList.forEach(distName ->{
                 String value = distName.getValue();
                 int intValue = Integer.parseInt(value);
@@ -26,27 +26,23 @@ public class Reporting {
             System.out.println("List of Incidents: ");
             incidentList.forEach(distName ->{
                 if (Integer.parseInt(inpValue) < Integer.parseInt(distName.getValue())) {
-                    System.out.println("\n Postcode:" + distName.getPostcode() + "\n Value: " + distName.getValue() + "\n Date: " + distName.getDate());
+                    System.out.println("\n Postcode:" + distName.getPostcode() + "\n Value: " + distName.getValue() + "\n Date: " + distName.getMonth() + "/" + distName.getYear());
                     System.out.println(" ");
                 }
             });
         }
 
         if (opt.equals("averageIncidentInYear")) {
-            ArrayList<Integer> totalValues = new ArrayList<Integer>();
+            ArrayList<Integer> totalValues = new ArrayList<>();
 
             Scanner s = new Scanner(System.in);
             System.out.println("--- Enter Year ---");
             System.out.print("Crime Year: ");
             String inpYear = s.nextLine();
 
-            System.out.println("List of Incidents: ");
             incidentList.forEach(distName ->{
-                String dateInfo = distName.getDate();
-                String yearInfo = dateInfo.substring(6, 10);
-                if (inpYear.equals(yearInfo)){
-                    //System.out.println("\n Postcode:" + distName.getPostcode() + "\n Value: " + distName.getValue() + "\n Date: " + distName.getDate());
-                    //System.out.println(" ");
+                String dateInfo = distName.getYear();
+                if (inpYear.equals(dateInfo)){
                     totalValues.add(Integer.parseInt(distName.getValue()));
                 }
             });
