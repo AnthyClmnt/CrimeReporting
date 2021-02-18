11299c365936671f1ad94319ee47841ef2b0f2aa
Anthony <Anthony.clermont@outlook.com>
2021-02-18 11:41:03 +0000

    Alteration to enterIncidentInfo function, to be able to work with the reworked Incident class. Creation of a fifth option from the menu, to allow testing easily, as-well as testing to see if a compile error occurs, when not properly entering data. This change also includes some javaDoc commenting.

--- src/ReportingIO.java
+++ src/ReportingIO.java
@@ -3,16 +3,31 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * This is a special class for representing a main object. This
+ *
+ *
+ * @author Anthony Clermont
+ *
+ */
+
 
 public class ReportingIO {
     public static void main(String[] args) throws FileNotFoundException {
-        District newDist1 = new District("Newcastle", "Murder");
-        District newDist2 = new District("Durham", "Robbery");
-        District newDist3 = new District("Durham", "Robbery");
+        District newDist1 = new District("Newcastle", 1);
+        District newDist2 = new District("Durham", 2);
+        District newDist3 = new District("Sunderland", 1);
 
-        Incident newInc1 = new Incident("NE4 5SN", "100", "10/02/2021");
-        Incident newInc2 = new Incident("NE4 9SN", "6700", "03/01/2021");
-        Incident newInc3 = new Incident("TS18 2SY", "12500", "27/11/2020");
+        Incident newInc1 = new Incident("NE4 5SN", "100", "02", "2020");
+        Incident newInc2 = new Incident("NE4 9SN", "6700", "03", "2021");
+        Incident newInc3 = new Incident("TS18 2SY", "12500", "11", "2021");
+
+        /*testing erroneous data to see if compile error occurs.
+        District errorDist = new District("Sunderland", "requires int value");
+        Incident errorInc = new Incident("TS18 2SY", 1234, "27/11/2020");
+         */
 
         List<District> districtList = new ArrayList<>();
         districtList.add(newDist1);
@@ -23,8 +38,6 @@ public class ReportingIO {
         incidentList.add(newInc1);
         incidentList.add(newInc2);
         incidentList.add(newInc3);
-
-        //System.out.println(incidentList + " " + districtList);
 
         Scanner s = new Scanner(System.in);
         boolean quit = false;
@@ -37,7 +50,8 @@ public class ReportingIO {
             switch (option) {
                 case "1" -> enterDistrictInfo(districtList);
                 case "2" -> enterIncidentInfo(incidentList);
-                case "3" -> search(districtList, incidentList);
+                case "3" -> statistics(incidentList);
+                case "5" -> test(districtList);
                 case "4" -> {
                     System.out.println("You chose to quit");
                     quit = true;
@@ -47,17 +61,45 @@ public class ReportingIO {
         }
     }
 
+    /**
+     * This function is called, as a test, allowing me to test the system.
+     * @param districtList, the function takes a list containing all the district objects
+     */
+
+    private static void test(List<District> districtList) {
+        districtList.forEach(name -> System.out.println(name.getDistrictName() +": "+ name.getIncident()));
+    }
+
+    /**
+     * This function is called when user wants to enter district information.
+     * @param districtList, the function takes a list containing all the district objects
+     */
+
     private static void enterDistrictInfo(List<District> districtList) {
         Scanner s = new Scanner(System.in);
         System.out.println("--- Enter District Info ---");
         System.out.print("District Name: ");
         String distName = s.nextLine();
-        System.out.print("Crime Type: ");
-        String crimeType = s.nextLine();
 
-        District newDistrict = new District(distName, crimeType);
-        districtList.add(newDistrict);
+        AtomicBoolean distFound = new AtomicBoolean(false);
+        districtList.forEach(name -> {
+            if (distName.equals(name.getDistrictName())) {
+                name.addIncident();
+                distFound.set(true);
+            }
+        });
+
+        String distFoundString = String.valueOf(distFound);
+        if (distFoundString.equals("false")) {
+            District newDistrict = new District(distName, 1);
+            districtList.add(newDistrict);
+        }
     }
+
+    /**
+     * This function is called when a user wants to enter incident information
+     * @param incidentList, the function takes a list containing all the incident objects
+     */
 
     private static void enterIncidentInfo(List<Incident> incidentList) {
         Scanner s = new Scanner(System.in);
@@ -66,31 +108,39 @@ public class ReportingIO {
         String postcode = s.nextLine();
         System.out.print("Value of Stolen Goods: £");
         String value = s.nextLine();
-        System.out.print("Date of Incident: ");
-        String date = s.nextLine();
+        System.out.print("Month of Incident (mm): ");
+        String month = s.nextLine();
+        System.out.print("Year of Incident (mm): ");
+        String year = s.nextLine();
 
-        Incident newIncident = new Incident(postcode, value, date);
+
+        Incident newIncident = new Incident(postcode, value, month, year);
         incidentList.add(newIncident);
     }
 
-    private static void search(List<District> districtList, List<Incident> incidentList) throws FileNotFoundException {
+    /**
+     * The function is called when the user wants to see statical analysis of the data
+     * @param incidentList, the function takes a list containing all the incident objects
+     */
+
+    private static void statistics(List<Incident> incidentList) {
         Scanner s = new Scanner(System.in);
         boolean returnToMenu = false;
         while (!returnToMenu) {
-            System.out.println("------Menu------\n" +
-                    "1: Highest Value Crime\n" +
-                    "2: Average Value Crime\n" +
-                    "3: All crimes > than £x\n" +
-                    "4: Return to main menu\n" +
-                    "----------------");
+            System.out.println("""
+                    ------Menu------
+                    1: Highest Value Crime
+                    2: Average Value Crime
+                    3: All crimes > than £x
+                    4: Return to main menu
+                    ----------------""");
 
             String option = s.nextLine();
             switch (option) {
                 case "1" -> new Reporting("highestValue", incidentList);
                 case "2" -> new Reporting("averageIncidentInYear", incidentList);
                 case "3" -> new Reporting("incident>£x", incidentList);
-                case "4" -> { returnToMenu = true;
-                }
+                case "4" -> returnToMenu = true;
                 default -> System.out.println("Invalid Option!!");
             }
         }
