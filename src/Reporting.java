ebcfb797c49999c9b557e538d3f3aee21ff43c48
Anthony <Anthony.clermont@outlook.com>
2021-02-18 21:05:05 +0000

    Added two functions, one which displays all incidents over a certain value, and the other which displays the district with the largest average value in a given year.

--- src/Reporting.java
+++ src/Reporting.java
@@ -2,12 +2,11 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class Reporting {
     public Reporting(String opt, List<Incident> incidentList, List<District> districtList) {
-        List<Incident> maxIncEachDist = new ArrayList<>();
         if (opt.equals("highestValue")) {
+            List<Incident> maxIncEachDist = new ArrayList<>();
             districtList.forEach(name -> {
                 maxIncEachDist.add(name.maxIncidentVal());
             });
@@ -22,44 +21,37 @@ public class Reporting {
                     "\n Value: £" + maxIncObj.getValue() + "\n Date (mm/yyyy): " + maxIncObj.getMonth() +"/"+ maxIncObj.getYear());
         }
 
-        /*
         if (opt.equals("incident>£x")) {
             Scanner s = new Scanner(System.in);
-            System.out.println("--- Enter Value ---");
-            System.out.print("Minimum Value: £");
-            String inpValue = s.nextLine();
+            System.out.println("Enter Amount: £");
+            String optAmount = s.nextLine();
+            System.out.println("-- Incident Values Greater than £" + optAmount + " --");
 
-            System.out.println("List of Incidents: ");
-            incidentList.forEach(distName ->{
-                if (Integer.parseInt(inpValue) < Integer.parseInt(distName.getValue())) {
-                    System.out.println("\n Postcode:" + distName.getPostcode() + "\n Value: " + distName.getValue() + "\n Date: " + distName.getMonth() + "/" + distName.getYear());
-                    System.out.println(" ");
+            incidentList.forEach(name -> {
+                int val = (int)name.getValue();
+                if (val > Integer.parseInt(optAmount)) {
+                    System.out.println("\n Postcode: " + name.getPostcode()+
+                            "\n Value: £" + name.getValue() + "\n Date (mm/yyyy): " + name.getMonth() +"/"+ name.getYear());
                 }
             });
         }
-
 
         if (opt.equals("averageIncidentInYear")) {
-            ArrayList<Integer> totalValues = new ArrayList<>();
-
+            List<Integer> avgIncValueEachDist = new ArrayList<>();
             Scanner s = new Scanner(System.in);
-            System.out.println("--- Enter Year ---");
-            System.out.print("Crime Year: ");
-            String inpYear = s.nextLine();
+            System.out.println("Enter Year (yyyy): ");
+            String optYear = s.nextLine();
 
-            incidentList.forEach(distName ->{
-                String dateInfo = distName.getYear();
-                if (inpYear.equals(dateInfo)){
-                    totalValues.add(Integer.parseInt(distName.getValue()));
-                }
+            districtList.forEach(name -> {
+                int distAvg = name.avgIncValueInYear(Integer.parseInt(optYear));
+                avgIncValueEachDist.add(distAvg);
             });
-            int totalSum = 0;
-            for (int value : totalValues) {
-                totalSum += value;
-            }
-            System.out.println("Average value for year " + inpYear + " is: £" + totalSum);
-        }
 
-         */
+            int maxAvgInc = Collections.max(avgIncValueEachDist);
+            int avgInc = avgIncValueEachDist.indexOf(maxAvgInc);
+            District avgIncDist = districtList.get(avgInc);
+            System.out.println("District with highest average value: " + avgIncDist.getDistrictName());
+
+        }
     }
 }
